3b2ddf7bbdb1d652b0e8cdef5fb428c7e1c967d6
flawmop <flawmop@insilicosoft.com>
2024-01-02 11:01:39 -0800

    Initializr import

---
+++ src/main/java/com/insilicosoft/portal/edgesvr/EdgeSvrApplication.java
@@ -0,0 +1,13 @@
+package com.insilicosoft.portal.edgesvr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdgeSvrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdgeSvrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/insilicosoft/portal/edgesvr/EdgeSvrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.insilicosoft.portal.edgesvr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdgeSvrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
