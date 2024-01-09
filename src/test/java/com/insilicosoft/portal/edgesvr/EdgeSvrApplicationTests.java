be1d5501559bbb8c1e15830de203791baaaa7109
flawmop <flawmop@insilicosoft.com>
2024-01-08 21:10:34 -0800

    Introduce mock bean to skip interaction with keycloak

--- src/test/java/com/insilicosoft/portal/edgesvr/EdgeSvrApplicationTests.java
+++ src/test/java/com/insilicosoft/portal/edgesvr/EdgeSvrApplicationTests.java
@@ -2,9 +2,14 @@ package com.insilicosoft.portal.edgesvr;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
 
 @SpringBootTest
 class EdgeSvrApplicationTests {
+
+        @MockBean
+        ReactiveClientRegistrationRepository clientRegistrationRepository;
 
 	@Test
 	void contextLoads() {
